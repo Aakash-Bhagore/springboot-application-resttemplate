4de08dabd8ab55b73bcc30da4ed4ccc98e41aa9b
Aakash-Bhagore <aakash.bhagore@webkorps.com>
2023-01-17 16:13:52 +0530

    GetForEntity all possible methods implemented using RestTemplate

--- src/main/java/com/springboot/resttemplate/controller/StudentGetForEntityController.java
+++ src/main/java/com/springboot/resttemplate/controller/StudentGetForEntityController.java
@@ -1,6 +1,7 @@
 package com.springboot.resttemplate.controller;
 
 import com.springboot.resttemplate.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +9,38 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(path = "/get")
 public class StudentGetForEntityController {
 
+  @Autowired
+  private RestTemplate restTemplate;
+
   //getting Student object back from resTemplate using getForEntity()
   @GetMapping(path = "/student/{studentId}")
   public ResponseEntity<Student> findStudent(@PathVariable("studentId") int studentId) {
-    RestTemplate restTemplate = new RestTemplate();
     ResponseEntity<Student> response = restTemplate.getForEntity("http://localhost:8585/students/"+studentId,Student.class);
     return ResponseEntity.ok(response.getBody());
+  }
+
+  @GetMapping(path = "/students")
+  public ResponseEntity<List<Student>> getStudents() {
+    ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:8585/students", List.class);
+    List<Student> students = response.getBody();
+    return ResponseEntity.ok(students);
+  }
+
+  @GetMapping(path = "/student")
+  public ResponseEntity<Student[]> getStudent_s() {
+    ResponseEntity<Student[]> response = restTemplate.getForEntity("http://localhost:8585/students", Student[].class);
+    return ResponseEntity.ok(response.getBody());
+  }
+
+  @GetMapping(path = "/students/{studentId}")
+  public ResponseEntity<String> getStudentByStudentId(@PathVariable("studentId") int studentId) {
+    String student = restTemplate.getForEntity("http://localhost:8585/student/"+studentId, String.class).getBody();
+    return ResponseEntity.ok(student!=null?student:null);
   }
 }
