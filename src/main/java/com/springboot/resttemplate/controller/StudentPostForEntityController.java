738947de7fa259ce3af4c1b78c116a38077bb8c4
Aakash-Bhagore <aakash.bhagore@webkorps.com>
2023-01-17 18:47:30 +0530

    post-for-entity and post-for-object implementation completed

--- src/main/java/com/springboot/resttemplate/controller/StudentPostForEntityController.java
+++ src/main/java/com/springboot/resttemplate/controller/StudentPostForEntityController.java
@@ -1,7 +1,30 @@
 package com.springboot.resttemplate.controller;
 
+import com.springboot.resttemplate.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
+@RequestMapping(path = "post-for-entity")
 public class StudentPostForEntityController {
+
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @PostMapping(path = "/student")
+  public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
+    ResponseEntity<Student> response = restTemplate.postForEntity("http://localhost:8585/student",student,Student.class);
+    return ResponseEntity.ok(response.getBody());
+  }
+
+  @PostMapping(path = "/students")
+  public ResponseEntity<String> saveStudents(@RequestBody Student student) {
+    ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8585/student",student,String.class);
+    return ResponseEntity.ok(response.getBody());
+  }
 }
--- src/main/java/com/springboot/resttemplate/controller/StudentPostForObjectController.java
+++ src/main/java/com/springboot/resttemplate/controller/StudentPostForObjectController.java
@@ -1,7 +1,30 @@
 package com.springboot.resttemplate.controller;
 
+import com.springboot.resttemplate.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
+@RequestMapping(path = "post-for-object")
 public class StudentPostForObjectController {
+
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @PostMapping(path = "/students")
+  public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
+    Student response = restTemplate.postForObject("http://localhost:8585/student",student,Student.class);
+    return ResponseEntity.ok(response!=null?response:null);
+  }
+
+  @PostMapping(path = "/student")
+  public ResponseEntity<String> saveStudents(@RequestBody Student student) {
+    String response = restTemplate.postForObject("http://localhost:8585/student",student,String.class);
+    return ResponseEntity.ok(!response.trim().isBlank()?response:null);
+  }
 }
